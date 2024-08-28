6f6c66193831a241647ac3bdb27759ac971d891b
BuildTools <unconfigured@null.spigotmc.org>
2024-08-28 11:22:14 +0200

    beta v3.9 update gradle

--- src/main/java/fr/gamity/launcher/thomas260913/Launcher.java
+++ src/main/java/fr/gamity/launcher/thomas260913/Launcher.java
@@ -42,7 +42,7 @@ import java.util.List;
 import java.util.UUID;
 
 public class Launcher extends Application {
-    private static final String version = "bêta v3.8.11";
+    private static final String version = "bêta v3.9";
     public static DiscordRichPresence presence = new DiscordRichPresence();
     public static DiscordRPC lib = DiscordRPC.INSTANCE;
     private static Launcher instance;
